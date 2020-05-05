825dc85c1c86ae2ee1e59a638b84ad2a38fb5819
Vinh Nguyen <themesexperts1986@gmail.com>
2020-05-05 23:34:19 +0700

    Refactor my Code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,24 +9,14 @@ function App() {
   const [headingText, setHeadingText] = useState("Hello"); // Init State
   const [buttonStyle, setButtonStyle] = useState(null);
 
-  function handleClick() {
-    // console.log("got Clicked");
-    setHeadingText("Button is Clicked");
-  }
-  function handleOut() {
-    setButtonStyle(null);
-  }
-  function handleMove() {
-    setButtonStyle(customStyleHover);
-  }
   return (
     <div className="container">
       <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
       <button
-        onClick={handleClick}
-        onMouseMove={handleMove}
-        onMouseOut={handleOut}
+        onClick={() => setHeadingText("is Clicked")}
+        onMouseMove={() => setButtonStyle(customStyleHover)}
+        onMouseOut={() => setButtonStyle(null)}
         style={buttonStyle}
       >
         Submit
